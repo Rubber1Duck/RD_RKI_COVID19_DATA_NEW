aa4b73fe9efe822e54b1926bc36946140d28cda6
Peter Markewitz <peter@markewitz.org>
2025-01-16 20:43:10 +0000

    add totaltime at end of loop

--- dateloop.sh
+++ dateloop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+STARTMAIN=`date +%s`
 startdate=$(date -I -d "$1") || exit -1
 enddate=$(date -I -d "$2")     || exit -1
 
@@ -9,3 +9,12 @@ while [ "$d" != "$enddate" ]; do
   d=$(date -I -d "$d + 1 day")
 done
 ./manual_update.sh $d
+
+ENDMAIN=`date +%s`
+TOTALMAIN=`expr $ENDMAIN - $STARTMAIN`
+TIMEMAIN=`date -d@$TOTALMAIN -u +%H:%M:%S`
+DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
+echo "*****************************************************************************************************"
+#     2025-01-15T12:30:00 : Update from 2020-04-08 to 2020-12-31 finished. Total execution time 00:00:00 .*
+echo "$DATE2 : Update from $startdate to $enddate finished. Total execution time $TIME .*"
+echo "*****************************************************************************************************"
